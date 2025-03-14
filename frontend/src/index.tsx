79205d326adbca319f08f109113cfebecfa30e8e
kuiqdeal <kuiqdeal@gmail.com>
2025-03-14 11:55:06 +0400

    Add frontend/src/index.tsx 1741938905424

---
+++ frontend/src/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import store from './store';
+import App from './App';
+import './i18n';
+import './styles/index.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find the root element');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
